385b612bd6341419031f80953052aaf3d9fd58e9
Alliny Silva Machado <alliny.machado@globalhitss.com.br>
2024-01-17 15:42:28 -0300

    loading details of movie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import './home.css';
 
-function Home(){
-  return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+// URL da API: /movie/now_playing?api_key=8505da56c688eee5b015b061e6dec8ae
+
+function Home() {
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "8505da56c688eee5b015b061e6dec8ae",
+          language: "pt-br",
+          page: 1,
+        }
+      })
+
+      setFilmes(response.data.results)
+      setLoading(false);
+    }
+
+    loadFilmes();
+
+  }, [])
+
+  if(loading) {
+    return(
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+
     </div>
   )
 }
